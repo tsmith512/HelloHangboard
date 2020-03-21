df1ef0d39529716d8ee3a90ddf348734f08d14ff
Taylor Smith <tsmith@tsmithcreative.com>
2020-03-21 13:46:17 -0500

    Props are a thing. And this is all Joe's fault anyway.

--- App.js
+++ App.js
@@ -7,7 +7,16 @@ export default class HelloHangboard extends Component {
       <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
         <Text style={{textAlign: "center", fontSize: 32}}>Hello, Hangboard.</Text>
         <Text style={{textAlign: "center"}}>I'm stuck in my apartment because of coronavirus.</Text>
+        <PropTest name="Joe" />
       </View>
     );
+  }
+}
+
+class PropTest extends Component {
+  render() {
+    return (
+      <Text style={{textAlign: "center"}}>This is all {this.props.name}'s fault!</Text>
+    )
   }
 }
