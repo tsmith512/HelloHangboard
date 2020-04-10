af0f5082312964d3e4fbc3a044f61363bc69e3af
Taylor Smith <tsmith@tsmithcreative.com>
2020-04-10 13:07:57 -0500

    Swap to a view for selective borders for better iOS support

--- components/About.js
+++ components/About.js
@@ -20,17 +20,19 @@ export default class About extends React.Component {
         <View style={aboutStyles.main}>
           <AntDesign style={aboutStyles.closeButton} name="closecircleo" size={32} color="black" onPress={this._exit} />
           <Title small />
-          <Text style={{marginTop: 8, marginBottom: 8}}>Sources:</Text>
+          <Text style={aboutStyles.paragraph}>Sources:</Text>
           <Link icon="link" title="REI's post “Hangboard Training 101”" href='https://www.rei.com/blog/climb/hangboard-training-101'></Link>
           <Link icon="link" title="Metolious Training Guide" href='https://www.metoliusclimbing.com/training-guide-overview.html'></Link>
-          <Text style={{marginTop: 8, marginBottom: 8}}>With special thanks to Joe and Jay.</Text>
-          <Text style={aboutStyles.warning}>
-            Improper use of a hangboard can cause serious finger and tendon
-            injury; don't strain to complete a set and do not train to the
-            failure point. Improper installation can cause a serious fall. Refer
-            to its setup and usage instructions as well as an experienced
-            trainer before use.
-          </Text>
+          <Text style={aboutStyles.paragraph}>With special thanks to Joe and Jay.</Text>
+          <View style={aboutStyles.warningContainer}>
+            <Text style={[aboutStyles.warningText, aboutStyles.paragraph]}>
+              Improper use of a hangboard can cause serious finger and tendon
+              injury; don't strain to complete a set and do not train to the
+              failure point. Improper installation can cause a serious fall. Refer
+              to its setup and usage instructions as well as an experienced
+              trainer before use.
+            </Text>
+          </View>
           <Link icon="hearto" title="Created by TSmith" href='https://tsmith.com' color='#CC0066'></Link>
         </View>
       </View>
@@ -61,10 +63,22 @@ const aboutStyles = StyleSheet.create({
     right: 16,
   },
 
-  warning: {
+  paragraph: {
     marginTop: 8,
+    marginBottom: 8,
+  },
+
+  warningContainer: {
+    marginTop: 8,
+    marginBottom: 8,
     borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#CCC',
     paddingTop: 8,
+    paddingBottom: 8,
+  },
+
+  warningText: {
     color: '#000000',
     fontWeight: 'bold',
   },
